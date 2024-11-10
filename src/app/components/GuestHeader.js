b2678e86e7c2eea199c22528c64cdc09416d5482
Andrea Ayon <ana88312@uga.edu>
2024-11-10 16:12:07 -0500

    guest header updated

--- src/app/components/GuestHeader.js
+++ src/app/components/GuestHeader.js
@@ -29,7 +29,7 @@ function GuestHeader({isLoggedIn, onLogin, onLogout, username}) {
             <header className="flex items-center justify-between p-2 bg-white">
                 <div className="flex items-center space-x-2">
                     <img src="/logo.svg" alt="Roaming Palette Logo" width="50" />
-                    <h1 className="font-extrabold text-black font-serif text-2xl pl-2">Roaming Palette</h1>
+                    <h1 className="font-extrabold text-black font-sans text-2xl pl-2">Roaming Palette</h1>
                 </div>
 
                 <div className="flex items-center justify-center flex-grow"> 
@@ -38,15 +38,15 @@ function GuestHeader({isLoggedIn, onLogin, onLogout, username}) {
                         value={searchQuery}
                         placeholder="Search restaurant, cuisines..."
                         onChange={(e) => setSearchQuery(e.target.value)}
-                        className="p-3 w-80 border border-gray-600 rounded-full focus:outline-none focus:ring-2 focus:ring-orange-600"
+                        className="p-3 w-80 border border-gray-600 rounded-full focus:outline-none focus:ring-2 focus:ring-orange-600 font-sans"
                     />
                 </div>
 
                 <div className="flex items-center space-x-4 z-10"> 
                     {!isLoggedIn ? (
                         <>
-                            <button className="p-3 hover:underline cursor-pointer bg-orange-600 rounded-lg text-black font-serif" onClick={loginHandler}>Sign Up</button>
-                            <button className="p-3 pr-4 pl-4 hover:underline cursor-pointer bg-orange-300 rounded-lg text-black font-serif" onClick={loginHandler}>Login</button>
+                            <button className="p-3 hover:underline cursor-pointer bg-orange-600 rounded-lg text-black font-sans font-semibold" onClick={loginHandler}>Sign Up</button>
+                            <button className="p-3 pr-4 pl-4 hover:underline cursor-pointer bg-orange-300 rounded-lg text-black font-sans font-semibold" onClick={loginHandler}>Login</button>
                         </>
                     ) : (
                         <>
@@ -97,7 +97,7 @@ function GuestHeader({isLoggedIn, onLogin, onLogout, username}) {
                 <img src="/header-image.svg" alt="header image" className="w-full h-full object-cover" />
                 <div className="absolute inset-0 bg-gradient-to-r from-transparent to-zinc-900 flex items-center justify-center">
                     <div className="w-2/4" />
-                    <h1 className="p-3 pr-6 text-6xl font-serif text-white text-right w-2/4 font-light">Explore <br /> the <br /> palettes of <br /> the world</h1>
+                    <h1 className="p-3 pr-6 text-8xl font-serif text-white text-right w-2/4 font-light">Explore <br /> the <br /> palettes of <br /> the world</h1>
                 </div>
             </div>
         </div>
@@ -105,3 +105,5 @@ function GuestHeader({isLoggedIn, onLogin, onLogout, username}) {
 }
 
 export default GuestHeader;
+
+
