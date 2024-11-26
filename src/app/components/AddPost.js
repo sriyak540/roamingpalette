bb099f4c15f13cb1c68ad2720513a6e58383e4b7
sriyak540 <120047824+sriyak540@users.noreply.github.com>
2024-11-25 20:33:48 -0500

    add post form

--- src/app/components/AddPost.js
+++ src/app/components/AddPost.js
@@ -9,35 +9,29 @@ const AddPost = ({username, onAddPost, onCancel}) => {
     const [selectedImage, setImage] = useState('');
     const [enteredLocation, setLocation] = useState('');
 
-    let enteredDescriptionToken = sessionStorage.getItem('enteredDescription');
-    let enteredTagsToken = sessionStorage.getItem('enteredTags'); 
-    let selectedImageToken = sessionStorage.getItem('selectedImage');
-    let enteredLocationToken = sessionStorage.getItem('enteredLocation');
+    // let enteredDescriptionToken = sessionStorage.getItem('enteredDescription');
+    // let enteredTagsToken = sessionStorage.getItem('enteredTags'); 
+    // let selectedImageToken = sessionStorage.getItem('selectedImage');
+    // let enteredLocationToken = sessionStorage.getItem('enteredLocation');
 
     const descriptionHandler = (event) => {
         setDescription(event.target.value);
-        sessionStorage.setItem('enteredDescription', enteredDescription);
+        // sessionStorage.setItem('enteredDescription', enteredDescription);
     };
     
     const tagHandler = (event) => {
         setTags(event.target.value);
-        sessionStorage.setItem('enteredTags', enteredTags);
+        // sessionStorage.setItem('enteredTags', enteredTags);
     };
 
-    const imageHandler = () => {
-        const selectedImage = prompt("Enter Image URL:");
-        if (selectedImage) {
-            setImage(selectedImage);
-            sessionStorage.setItem('selectedImage', selectedImage);
-        }
+    const imageHandler = (event) => {
+        setImage(event.target.value);
+        // sessionStorage.setItem('selectedImage', selectedImage);
     };
 
-    const locationHandler = () => {
-        const enteredLocation = prompt("Enter Location:");
-        if (enteredLocation) {
-            setLocation(enteredLocation);
-            sessionStorage.setItem('enteredLocation', enteredLocation);
-        }
+    const locationHandler = (event) => {
+        setLocation(event.target.value);
+        // sessionStorage.setItem('enteredLocation', enteredLocation);
     };
 
     const submitHandler = (event) => {
@@ -45,20 +39,20 @@ const AddPost = ({username, onAddPost, onCancel}) => {
 
         const post_data = {
             username: postingUser,
-            location: enteredLocationToken,
-            description: enteredDescriptionToken,
-            tags: enteredTagsToken,
-            image: selectedImageToken
+            location: enteredLocation,
+            description: enteredDescription,
+            tags: enteredTags,
+            image: selectedImage
         };
 
         onAddPost(post_data); // Call parent component's function to add post to the list
         console.log(post_data); // Log post data to console for verification
         onCancel();
+
         // clear form
-        setPostingUser('');
+        //setPostingUser('');
         setDescription('');
         setTags('');
-        //useSelectedLayoutSegments('');
         setImage('');
         setLocation('');
     };
@@ -67,23 +61,23 @@ const AddPost = ({username, onAddPost, onCancel}) => {
         <div className="p-5 flex justify-center items-center">
             <Card className="w-full max-w-2xl p-6 bg-white rounded-lg shadow-lg">
                 <form onSubmit={submitHandler} className="space-y-6">
-                <div className="flex justify-between items-center">
-            {/* Cancel Button */}
-            <button
-                type="button"
-                onClick={onCancel}
-                className="text-sm text-gray-500 hover:text-gray-700"
-            >
-                X
-            </button>
+                    <div className="flex justify-between items-center">
+                        {/* Cancel Button */}
+                        <button
+                            type="button"
+                            onClick={onCancel}
+                            className="text-sm text-gray-500 hover:text-gray-700"
+                        >
+                            X
+                        </button>
 
-            {/* Submit Button */}
-            <button 
-                type="submit" 
-                className="py-2 px-6 bg-teal-700 text-white rounded-lg hover:bg-teal-600">
-                Post
-            </button>
-        </div>
+                        {/* Submit Button */}
+                        <button 
+                            type="submit" 
+                            className="py-2 px-6 bg-teal-700 text-white rounded-lg hover:bg-teal-600">
+                            Post
+                        </button>
+                    </div>
                     {/* Username */}
                     <div className="inline-flex text-left">
                         <span className="p-6 mb-4 cursor-pointer bg-orange-600 rounded-full border-2 border-gray-400"></span>
@@ -110,24 +104,25 @@ const AddPost = ({username, onAddPost, onCancel}) => {
                             rows="1"
                         />
                     </div>
-
-                    {/* Action Buttons */}
-                    <div className="flex justify-start space-x-4">
-                        {/* Add Image Button */}
-                        <button 
-                            type="button" 
-                            onClick={imageHandler} 
-                            className="py-2 px-4 bg-teal-700 text-white rounded-lg hover:bg-teal-600">
-                            Add Image
-                        </button>
-
-                        {/* Add Location Button */}
-                        <button 
-                            type="button" 
-                            onClick={locationHandler} 
-                            className="px-4 py-2 bg-teal-700 text-white rounded-lg hover:bg-teal-600">
-                            Add Location
-                        </button>
+                    {/* Image */}
+                    <div>
+                        <textarea
+                            value={selectedImage}
+                            onChange={imageHandler}
+                            placeholder="Enter image URL..."
+                            className="w-full p-3 mt-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                            rows="1"
+                        />
+                    </div>
+                    {/* Location */}
+                    <div>
+                        <textarea
+                            value={enteredLocation}
+                            onChange={locationHandler}
+                            placeholder="Enter tags here"
+                            className="w-full p-3 mt-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                            rows="1"
+                        />
                     </div>
                 </form>
             </Card>
